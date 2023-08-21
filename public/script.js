af52c1ab1912c1849ca771999fc631fc6f3b0dbc
JOYSTON <116254639+Joystonm@users.noreply.github.com>
2023-08-21 23:02:41 +0530

    form

--- public/script.js
+++ public/script.js
@@ -30,7 +30,7 @@ subsrcibe.addEventListener("click",(e)=>{
     var submitbtn = document.getElementById("submitbtn");
     submitbtn.addEventListener("click", function (e) {
         e.preventDefault();
-        var form = document.querySelector("form");
+        var form = document.getElementById("form");
         var popmessage = document.getElementById("pop-message");
 
         var name = document.getElementById("name").value.trim();
@@ -43,20 +43,20 @@ subsrcibe.addEventListener("click",(e)=>{
         var emailpattern = /^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/;
         var phonepattern = /^\+{0,2}(\-|\.|\s)?(\(?\d{0,3}\))?(\-|\.|\s)?\d{3}(\-|\.|\s)?\d{4}$/g;
 
-        if (name === "" || email === "" || phone === "" || comment === "") {
-            popmessage.textContent = "Please fill out all fields";
-            return;
-        }
+        // if (name === "" || email === "" || phone === "") {
+        //     popmessage.textContent = "Please fill out all fields";
+        //     return;
+        // }
 
-        if (!emailpattern.test(email)) {
-            popmessage.textContent = "Please enter a valid email address";
-            return;
-        }
+        // if (!emailpattern.test(email)) {
+        //     popmessage.textContent = "Please enter a valid email address";
+        //     return;
+        // }
 
-        if (!phonepattern.test(phone)) {
-            popmessage.textContent = "Please enter a valid phone number";
-            return;
-        }
+        // if (!phonepattern.test(phone)) {
+        //     popmessage.textContent = "Please enter a valid phone number";
+        //     return;
+        // }
 
         form.reset();
         popmessage.textContent = "Submitted successfully!";
